22f267b5bcfdd279d3e52c4e01c6c23f7acf2e20
Jeroen Mies <jeroen.mies@gmail.com>
2017-10-17 22:48:14 +0200

    Added searching Youtube

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
+import _ from "lodash";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
@@ -30,9 +31,13 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce((searchTerm) => {
+            this.videoSearch(searchTerm)
+        }, 300);
+
         return (
             <div>
-                <SearchBar onSearchTermChange={searchTerm => this.videoSearch(searchTerm)}/>
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
