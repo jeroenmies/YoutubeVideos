7999d29bc783db3aa34f4b72bba9322a43be3c45
Jeroen Mies <jeroen.mies@gmail.com>
2017-10-17 17:35:47 +0200

    Show 5 items

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
 const API_KEY = "AIzaSyDylrLJf9DHDMo6dpSWhuRZkIm_hd8AqDo";
 
@@ -21,6 +22,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
