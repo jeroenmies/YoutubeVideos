f7d403173bd294e303cfc61cf27cb18fbe81ccf1
Jeroen Mies <jeroen.mies@gmail.com>
2017-10-17 17:12:04 +0200

    Initial search built in

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 
 const API_KEY = "AIzaSyDylrLJf9DHDMo6dpSWhuRZkIm_hd8AqDo";
 
-// Create a new component.
-// This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'rijkswaterstaat'}, (videos) => {
+            this.setState({videos}); // this.setState({ videos: videos })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
 }
 
-// Take this component's generated HTML
-// and put it on the page (in the DOM)
 ReactDOM.render(<App/>, document.querySelector(".container"));
