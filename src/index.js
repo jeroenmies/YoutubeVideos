137e2fdedb52f3b57f394b43724a44b3eecf28a5
Jeroen Mies <jeroen.mies@gmail.com>
2017-10-17 22:14:07 +0200

    Showing selected video

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = "AIzaSyDylrLJf9DHDMo6dpSWhuRZkIm_hd8AqDo";
 
@@ -11,10 +12,16 @@ class App extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'rijkswaterstaat'}, (videos) => {
-            this.setState({videos}); // this.setState({ videos: videos })
+            this.setState({
+                videos: videos,  // this.setState({ videos })
+                selectedVideo: videos[0],
+            });
         });
     }
 
@@ -22,6 +29,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList videos={this.state.videos}/>
             </div>
         );
