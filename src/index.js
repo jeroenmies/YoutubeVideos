35d5ad49dce132bdf214a314f6449e235ef5e62d
Jeroen Mies <jeroen.mies@rws.nl>
2020-03-12 13:25:43 +0100

    updated packages
    Signed-off-by: Jeroen Mies <jeroen.mies@rws.nl>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
+import _ from "lodash";
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
-import _ from "lodash";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
 
 const API_KEY = "AIzaSyDylrLJf9DHDMo6dpSWhuRZkIm_hd8AqDo";
-
 
 class App extends Component {
     constructor(props) {
